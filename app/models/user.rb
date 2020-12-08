b7c6b916d83c11c07ad9e3a060154a06189bd73b
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-08 11:42:33 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :f_name,:l_name,:nickname,:birthday, presence: true
+  validates :f_kananame,:l_kananame, presence: true,
+    format: {
+    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: "全角カタカナのみで入力して下さい"
+    }
+    validates :f_name,:l_name, presence: true,
+    format: {
+    with: /\A[ぁ-んァ-ン一-龥]+\z/,
+    message: "全角で漢字、ひらがな、かたかなのみで入力して下さい"
+    }
 end
