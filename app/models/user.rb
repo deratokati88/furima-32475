62a232716f1e5fef2a84bab8bd7d9cf7a4aced80
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-08 19:26:27 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :f_name,:l_name,:nickname,:birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+  validates :nickname,:birthday, presence: true
+
   # validates :f_kananame,:l_kananame, presence: true,
   #   format: {
   #   with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
   #   message: "全角カタカナのみで入力して下さい"
   # }
 
-  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: 'に全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'に全角カタカナを使用してください' } do
     validates :f_kananame
     validates :l_kananame
   end
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   #   message: "全角で漢字、ひらがな、かたかなのみで入力して下さい"
   # }
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角で漢字、ひらがな、かたかなのみで入力して下さい' } do
     validates :f_name
     validates :l_name
   end
