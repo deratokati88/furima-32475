9bfb28defd33078b8808cff73e4f96cc9441f82c
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-09 18:14:42 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :status, :shipping_charge, :prefecture, :shipping_time
+  belongs_to :user 
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_time
+
 
   validates :name, :description, :price, presence: true
-  validates :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_time_id numericality: { other_than: 1 } 
+  validates :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_time_id, numericality: { other_than: 1 } 
 end
