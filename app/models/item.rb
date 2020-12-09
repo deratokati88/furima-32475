948c80a67bfc444a44ded81ad4ca6b258bc5e54b
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-09 19:02:02 +0900

    商品追加機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :shipping_time
-
+  has_one_attached :image
 
   validates :name, :description, :price, presence: true
   validates :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_time_id, numericality: { other_than: 1 } 
