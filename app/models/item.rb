53e4d221315e81e1206fc752135f0779b1980367
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-09 17:12:09 +0900

    アイテムモデルのアソシエーションとバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :shipping_charge, :prefecture, :shipping_time
+
+  validates :name, :description, :price, presence: true
+  validates :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_time_id numericality: { other_than: 1 } 
 end
