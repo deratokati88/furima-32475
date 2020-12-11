161fb475b1fa5402285231ca99513e3be4322e47
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-11 16:23:12 +0900

    不要なバリデーションの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,6 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :name, :description, :price, :image, presence: true
-  validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字を入力してください' }, numericality: { only_integer: true,
-                                                                                              greater_than: 299, less_than: 10_000_000 }
+  validates :price, numericality: { only_integer: true,greater_than: 299, less_than: 10_000_000 }
   validates :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_time_id, numericality: { other_than: 1 }
 end
