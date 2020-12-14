1b48d0ad8ddcd9851561241fd983ca0736d5b478
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-14 14:37:02 +0900

    アップデートアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,20 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
   end
+  
 end
