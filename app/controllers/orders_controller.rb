a0741b61829a10d697f4991312880728f2740727
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-16 15:55:56 +0900

    商品購入者の情報を保存できるように

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,25 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
+    @buyer_shipping = BuyerShipping.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @buyer_shipping = BuyerShipping.new(order_params)   
+     if @buyer_shipping.valid?
+      @buyer_shipping.save
+      redirect_to root_path
+     else
+       render :index
+     end
+  end
+
+  private
+
+  def order_params
+    params.require(:buyer_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
