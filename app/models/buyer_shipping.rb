c1b35737ba56a4ff023b55b270798a60153a380a
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-16 15:56:13 +0900

    バリデーション を設定

---
+++ app/models/buyer_shipping.rb
@@ -0,0 +1,21 @@
+class BuyerShipping
+  include ActiveModel::Model
+  attr_accessor :item_id, :buyer_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :phone, format: {with: /\A[0-9]{9,11}+\z/, message: "is invalid. Input half-width characters."}
+    validates :user_id
+    validates :item_id
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
+  
+  def save
+    buyer = Buyer.create(item_id: item_id,user_id: user_id)
+    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone: phone, buyer_id: buyer.id)
+  end
+end
