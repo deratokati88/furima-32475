873979cecadebd91321f6cb914b1fa388e134056
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-16 16:06:01 +0900

    購入画面への遷移を制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :item_idetify
+  before_action :move_to_index
   def index
-    @item = Item.find(params[:item_id])
+
     @buyer_shipping = BuyerShipping.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buyer_shipping = BuyerShipping.new(order_params)   
      if @buyer_shipping.valid?
       @buyer_shipping.save
@@ -18,6 +18,14 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def item_idetify
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user.id
+  end
 
   def order_params
     params.require(:buyer_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
