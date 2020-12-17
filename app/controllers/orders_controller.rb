b4c51af11f0d714a143e4a00205aa6410b80db2e
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-17 19:59:43 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,13 +3,13 @@ class OrdersController < ApplicationController
   before_action :item_idetify
   before_action :move_to_index
   def index
-
     @buyer_shipping = BuyerShipping.new
   end
 
   def create
     @buyer_shipping = BuyerShipping.new(order_params)   
      if @buyer_shipping.valid?
+      pay_item
       @buyer_shipping.save
       redirect_to root_path
      else
@@ -24,10 +24,19 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id == @item.user.id
+    redirect_to root_path if (current_user.id == @item.user.id) || (Buyer.where(item_id: @item.id).exists?)
   end
 
   def order_params
-    params.require(:buyer_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buyer_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: order_params[:token],    
+      currency: 'jpy'                 
+    )
   end
 end
--- app/models/buyer_shipping.rb
+++ app/models/buyer_shipping.rb
@@ -1,6 +1,6 @@
 class BuyerShipping
   include ActiveModel::Model
-  attr_accessor :item_id, :buyer_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone
+  attr_accessor :item_id, :buyer_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,6 +10,7 @@ class BuyerShipping
     validates :phone, format: {with: /\A[0-9]{9,11}+\z/, message: "is invalid. Input half-width characters."}
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   validates :prefecture_id, numericality: { other_than: 1 }
