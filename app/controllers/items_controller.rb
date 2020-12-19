0dee821c5ab9c474ddac2d42a925716fe6976cd3
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2020-12-19 10:50:15 +0900

    空のコミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :item_idetify, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
+  
   def index
     @items = Item.order('created_at DESC')
   end
